98d412f5d84385c493bce6ce971045c074ce4bef
SowmiyaIyyappan <sowmiyaselvi19@gmail.com>
2026-05-27 13:59:17 +0530

    ashu complete work

--- src/org/gitdemo/SampleGit.java
+++ src/org/gitdemo/SampleGit.java
@@ -2,7 +2,8 @@ package org.gitdemo;
 
 public class SampleGit {
 	public static void main(String[] args) {
-		System.out.println("Git code added");
+		//System.out.println("Git code added");
+		System.out.println("Ashu code added");
 	}
 
 }
