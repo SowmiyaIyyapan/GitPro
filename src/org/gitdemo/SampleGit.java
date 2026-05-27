7d98797c79ef521ae024c2046d564af1289f9d92
SowmiyaIyyappan <sowmiyaselvi19@gmail.com>
2026-05-27 14:08:23 +0530

    Ahana completed her work

--- src/org/gitdemo/SampleGit.java
+++ src/org/gitdemo/SampleGit.java
@@ -2,7 +2,7 @@ package org.gitdemo;
 
 public class SampleGit {
 	public static void main(String[] args) {
-		System.out.println("Git code added");
+		System.out.println("Ahana code added ");
 	}
 
 }
